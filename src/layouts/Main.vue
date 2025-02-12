cfa5da973bcf3c5d80cf72f17da1aa46e7c609f6
hai.nguyen <hai.nguyen@codihaus.com>
2025-02-12 15:42:39 +0700

    update

--- src/layouts/Main.vue
+++ src/layouts/Main.vue
@@ -1,18 +1,9 @@
 <template>
-	<div class="min-h-screen bg-gray-100 flex flex-col">
-		<header class="p-4 flex justify-end">
-			<!-- <LanguageSwitcher /> -->
-		</header>
+	<div class="min-h-screen flex flex-col">
 		<main class="flex-1 flex justify-center items-center">
 			<router-view />
 		</main>
-		<footer>
-			<!-- <Footer /> -->
-		</footer>
 	</div>
 </template>
 
-<script lang="ts" setup>
-import Footer from "@/layouts/Footer.vue";
-import LanguageSwitcher from "@/components/Language.vue";
-</script>
+<script lang="ts" setup></script>
